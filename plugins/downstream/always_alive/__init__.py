ce77f5fce485617892ad0b7d1364ff5bd47e1b39
Akiva Silver <akiva.silver@gmail.com>
2019-04-21 15:47:41 -0400

    refactored downstream plugins refactored plugin class

---
+++ plugins/downstream/always_alive/__init__.py
@@ -0,0 +1,52 @@
+#      Copyright (C) 2019 - 2019 Akiva Silver and contributors of Queue Plus
+#      GitHub Page: <https://github.com/the-emperium/queue-plus>
+#
+#      This file (__init__.py) is part of Queue Plus.
+#
+#      Queue Plus is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      any later version.
+#
+#      Queue Plus is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with Queue Plus.  If not, see <https://www.gnu.org/licenses/>.
+from plugins.downstream import DownstreamPlugin
+
+
+class AlwaysAlivePlugin(DownstreamPlugin):
+	def __init__(self, *args, **kwargs):
+		super(AlwaysAlivePlugin, self).__init__(*args, **kwargs)
+		
+		self.alive_task = None
+	
+	
+	def on_join(self):
+		self.alive_task = self.ticker.add_loop(20, self.update_keep_alive)
+		return
+	
+	
+	def on_leave(self):
+		self.ticker.remove(self.alive_task)
+		return
+	
+	def update_keep_alive(self):
+		# 1.7.x
+		if self.config["version"] <= 338:
+			payload = self.buff_type.pack_varint(0)
+		
+		# 1.12.2
+		else:
+			payload = self.buff_type.pack('Q', 0)
+		
+		self.send_packet("keep_alive", payload)
+		return
+	
+	# discards all the keep alive packets sent
+	def packet_keep_alive(self, buff):
+		buff.discard()
+		return "break"
---
+++ plugins/downstream/player_info/__init__.py
@@ -0,0 +1,31 @@
+#      Copyright (C) 2019 - 2019 Akiva Silver and contributors of Queue Plus
+#      GitHub Page: <https://github.com/the-emperium/queue-plus>
+#
+#      This file (__init__.py) is part of Queue Plus.
+#
+#      Queue Plus is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      any later version.
+#
+#      Queue Plus is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with Queue Plus.  If not, see <https://www.gnu.org/licenses/>.
+from plugins.downstream import DownstreamPlugin
+
+
+class PlayerInfoPlugin(DownstreamPlugin):
+	def __init__(self, *args, **kwargs):
+		super(PlayerInfoPlugin, self).__init__(*args, **kwargs)
+		
+		self.player_username = None
+		self.player_uuid = None
+		
+	def on_join(self):
+		self.player_username = self.protocol.display_name
+		self.player_uuid = self.protocol.uuid
+		return
