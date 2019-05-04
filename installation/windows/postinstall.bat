bcca7c894cbe532a8b982ea5553cddd2b0c3e5c8
Akiva Silver <akiva.silver@gmail.com>
2019-05-03 23:48:58 -0400

    created post install batch

---
+++ installation/windows/postinstall.bat
@@ -0,0 +1,20 @@
+echo off
+set python_folder=%~1
+
+"%python_folder%\Scripts\pip.exe" install -r .\installation\windows\requirements.txt
+"%python_folder%\Scripts\pip.exe" install .\installation\windows\Twisted-19.2.0-cp37-cp37m-win32.whl
+"%python_folder%\Scripts\pip.exe" install quarry
+
+mkdir .\data
+copy ".\bin\sources\accounts.csv" ".\data\accounts.csv"
+copy ".\bin\sources\config.json" ".\data\config.json"
+
+echo echo off > ".\start.bat"
+echo echo ########################################################## >> ".\start.bat"
+echo echo ##                                                      ## >> ".\start.bat"
+echo echo ##                  CTRL + C to Exit                    ## >> ".\start.bat"
+echo echo ##                                                      ## >> ".\start.bat"
+echo echo ########################################################## >> ".\start.bat"
+echo %python_folder%\python.exe start.py >> ".\start.bat"
+
+rmdir .\installation /q /s
